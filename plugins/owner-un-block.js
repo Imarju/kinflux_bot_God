d0b8c5697051b3037392e583faecba5be6769f0f
Imarju <138795356+Imarju@users.noreply.github.com>
2023-09-11 18:11:26 +0530

    Update owner-un-block.js

--- plugins/owner-un-block.js
+++ plugins/owner-un-block.js
@@ -15,7 +15,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
         else conn.reply(m.chat, why, m, { mentions: [m.sender] })
         break;
     }
-    if (res[0]) conn.reply(m.chat, `*SUCCESS! USER ${command} ACTION PERFORMED ON ${res ? `${res.map(v => '@' + v.split("@")[0])}` : ''}*`, m, { mentions: res })
+    if (res[0]) conn.reply(m.chat, `*🤝 SUCCESS! USER ${command} ACTION PERFORMED ON ${res ? `${res.map(v => '@' + v.split("@")[0])}` : ''}*`, m, { mentions: res })
   }
   
   handler.command = /^(block|unblock)$/i
