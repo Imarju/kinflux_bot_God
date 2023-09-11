0c0e9ae35d111e33b8610583aea5d9b00c860938
Imarju <138795356+Imarju@users.noreply.github.com>
2023-09-11 16:27:11 +0530

    Update tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -4,7 +4,7 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw '✳️ Respond to an image/video'
+  if (!mime) throw '😑 Respond to an image/video Bruhhhhhhh'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
