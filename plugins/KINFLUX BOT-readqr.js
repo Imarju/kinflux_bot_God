809e4c1f3aa2cd371c6fe8fc22d1908950236b4b
Imarju <138795356+Imarju@users.noreply.github.com>
2023-09-11 20:49:32 +0530

    Update KINFLUX BOT-readqr.js

--- plugins/KINFLUX BOT-readqr.js
+++ plugins/KINFLUX BOT-readqr.js
@@ -3,11 +3,11 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw '*Respond to a qr*'
+if (!mime) throw '*😑Respond to a qr*'
 let img = await q.download?.()
 let url = await uploadImage(img)
 let anu = await fetch(`https://api.lolhuman.xyz/api/read-qr?apikey=${lolkeysapi}&img=${url}`)
 let json = await anu.json()
-await m.reply(`*here u go:* ${json.result}`)}
+await m.reply(`*😎 here u go:* ${json.result}`)}
 handler.command = /^(readqr)$/i
 export default handler
