ef8d990e1f8f7bb5abc4a4d3eea53103fbb217dd
Imarju <138795356+Imarju@users.noreply.github.com>
2023-09-11 16:53:19 +0530

    Update tools-google.js

--- plugins/tools-google.js
+++ plugins/tools-google.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command, args }) => {
     const fetch = (await import('node-fetch')).default
     let full = /f$/i.test(command)
     let text = args.join` `
-    if (!text) return conn.reply(m.chat, '✳️ What do you want to search on Google?', m)
+    if (!text) return conn.reply(m.chat, '🌝 haa bro Google mein kyaa search karna chahta hai akele akele maje karega kya 🌝?', m)
     let url = 'https://google.com/search?q=' + encodeURIComponent(text)
     let search = await googleIt(text)
     let msg = search.articles.map(({
