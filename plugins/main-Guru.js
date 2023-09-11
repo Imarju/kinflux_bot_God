482fd4f808a5290164abcc2bde183f26858bc622
Imarju <138795356+Imarju@users.noreply.github.com>
2023-09-11 23:51:13 +0530

    Add files via upload

---
+++ plugins/main-Guru.js
@@ -0,0 +1,31 @@
+let handler = async m => m.reply(`
+
+≡  *gmx ┃ ᴮᴼᵀ*   GROUPS
+
+─────────────
+▢ Join public bot group and support
+
+
+▢ Group 2
+
+
+≡ Disabled links? enter here! 
+
+▢ Group WhatsApp 
+ 
+─────────────
+▢ *Owner Telegram*
+ 
+
+▢ *owner number*
+• https://wa.me/919131652091
+• https://wa.me/919131652091
+
+-----------------------------------
+
+`.trim())
+handler.help = ['gpguru']
+handler.tags = ['main']
+handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
+
+export default handler
