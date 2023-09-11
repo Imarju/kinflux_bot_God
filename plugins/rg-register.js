af2e24b37ad68383aa3f9f6c3d6e079fbfccd217
Imarju <138795356+Imarju@users.noreply.github.com>
2023-09-11 18:01:43 +0530

    Update rg-register.js

--- plugins/rg-register.js
+++ plugins/rg-register.js
@@ -5,22 +5,22 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) throw `✳️ You are already registered\n\nDo you want to re-register?\n\n 📌 Use this command to remove your record \n*${usedPrefix}unreg* <Serial number>`
-  if (!Reg.test(text)) throw `⚠️ Format incorrect\n\n ✳️ Use this command: *${usedPrefix + command} name.age*\n📌Exemple : *${usedPrefix + command}* ${name2}.16`
+  if (user.registered === true) throw `bro😑 you are already registered\n\nDo you want to re-register?\n\n 📌 Use this command to remove your record \n*${usedPrefix}unreg* <Serial number>`
+  if (!Reg.test(text)) throw `🤡 format incorrect\n\n 👻 Use this command: *${usedPrefix + command} name.age*\n📌Exemple : *${usedPrefix + command}* ${name2}.16`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw '✳️ The name cannot be empty'
-  if (!age) throw '✳️ age cannot be empty'
-  if (name.length >= 30) throw '✳️The name is too long' 
+  if (!name) throw '😑bruhh the name cannot be empty'
+  if (!age) throw '💀bruhh age cannot be empty'
+  if (name.length >= 30) throw '💀💀💀💀💀abey ye kon sa naam hai itna lamba naam hai tumhara' 
   age = parseInt(age)
-  if (age > 100) throw '👴🏻 Wow grandpa wants to play bot'
-  if (age < 5) throw '🚼  there is a grandpa baby jsjsj '
+  if (age > 100) throw '👴🏻 arey dada jee Aapka toh umar ho gaya hai jao jaake dusri saadi kar lo'
+  if (age < 5) throw 'arey tum toh abhi chhote ho tumhari lulli bhi chhoti hai jao jake lollipop khao 😑'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-┌─「 *REGISTERED* 」─
+┌─「 *GMX REGISTER* 」─
 ▢ *NUMBER:* ${name}
 ▢ *AGE* : ${age} years
 ▢ *SERIEL NUMBER* :
