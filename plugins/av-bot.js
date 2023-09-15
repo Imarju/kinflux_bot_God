101bebe58256bbbf8d369763f1f00a2c7fa28247
Imarju <138795356+Imarju@users.noreply.github.com>
2023-09-15 10:20:59 +0530

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -5,7 +5,7 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./src/mp3/${pickRandom(["Arju", "Arju1", "Arju2", "Arju3", "Arju4"])}.mp3`
 
-m.reply( `Hello ${taguser} Do u want to use Official GMX BOT type /help `)
+m.reply( `Hello ${taguser} Do u want to use Official GMX BOT type /help and support me on Instagram and get your owe bot free my Instagram id : https://instagram.com/gmx_grand_master_x?igshid=YTQwZjQ0NmI0OA==`)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
