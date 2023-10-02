096ba1269dd1be0ea503ffb8edd9ae3c520bc50c
Imarju <138795356+Imarju@users.noreply.github.com>
2023-10-02 19:21:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['919131652091', 'arju', true],
+  ['918235857980', 'supreme', true],
   ['919131652091', 'gmx official', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['919131652091','919131652091'] 
-global.prems = ['919131652091', '919131652091', '919131652091']
+global.mods = ['919131652091','918235857980'] 
+global.prems = ['919131652091','918235857980']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
