cbd9c804f1d4d748cfd10b6399a42704c213104b
Imarju <138795356+Imarju@users.noreply.github.com>
2023-10-04 15:50:56 +0530

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -24,5 +24,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' Day(s) ️', h, ' Hour(s) ', m, ' Minute(s)'].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' Day(s) ️', h, ' Hour(s) ', m, ' Minute(s)', s, ' Second(s)'].map(v => v.toString().padStart(2, 0)).join('')
 }
