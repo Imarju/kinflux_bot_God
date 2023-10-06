6591bb2c3db30c63d70e1da2cd6550800c0c45a7
Imarju <138795356+Imarju@users.noreply.github.com>
2023-10-06 16:40:04 +0530

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -5,11 +5,11 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./src/mp3/${pickRandom(["Arju", "Arju1", "Arju2", "Arju3", "Arju4"])}.mp3`
 
-m.reply( `Hello ${taguser} Do u want to use Official GMX BOT type /help and support me on Instagram and get your owe bot free my Instagram id : https://instagram.com/gmx_grand_master_x?igshid=YTQwZjQ0NmI0OA==`)
+m.reply( `Hey parth! ${taguser} Do u want to use BOT type /help and support me on Instagram and get your own bot 🌿 Instagram id : https://instagram.com/gmx_grand_master_x?igshid=YTQwZjQ0NmI0OA==`)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(bot|arju)$/i
+handler.customPrefix = /^(bot|hey parth|arju)$/i
 handler.command = new RegExp
 
 export default handler
