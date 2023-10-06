0296ae6732432bf0e308eb7a7f7c9786f09d9999
Imarju <138795356+Imarju@users.noreply.github.com>
2023-10-06 16:22:17 +0530

    Update main-Guru.js

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -1,32 +1,33 @@
 let handler = async m => m.reply(`
 
-≡  *gmx ┃ ᴮᴼᵀ*   GROUPS
+≡ *𝐓𝐇𝐄-𝐎𝐅𝐅𝐈𝐂𝐈𝐀𝐋-𝐆𝐌𝐗-𝐌𝐃* 
 
 ─────────────
-▢ Join public bot group and support
+▢	*GROUP 1 ®*				
 
-https://instagram.com/gmx_grand_master_x?igshid=YTQwZjQ0NmI0OA==
+https://chat.whatsapp.com/HvHmJcC7tOb9F4aOJFyeVF
 
-▢ Group 2
-_pending..._
+▢ *GROUP DESCRIPTION* 
+Hello friends, all of you are welcome here. You can use public bots in this group and became GMX premium users for only Rs 15. You can also tell us new ideas so that we can upgrade the features of the bot. If you want to do our bot yourself or customize it for your group, then you will get all these services for only 20 rupees. You have to scan the QR given in bot info to make the payment. After that you have to contact our developer, thank you have a yal day. 
 
 ≡ For create a new bot for ur own, let's chat on Instagram by follow us 
 
 ▢ https://instagram.com/gmx_grand_master_x?igshid=YTQwZjQ0NmI0OA==
  
 ─────────────
-▢ *Owner Telegram*
- _pending..._
+▢ /info bot 
+ 
 
-▢ *owner number*
+▢ *developer number*
 • https://wa.me/919131652091
-• https://wa.me/919131652091
+▢ *developer email*
+• grandmasterx886@gmail.com
 
 -----------------------------------
 
 `.trim())
 handler.help = ['gpguru']
 handler.tags = ['main']
-handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
+handler.command = ['groups', 'groupguru', 'gugp', 'gmx', 'ggp', 'gpguru'] 
 
 export default handler
